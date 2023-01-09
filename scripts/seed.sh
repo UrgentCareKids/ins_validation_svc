db5189bc5f324e5549d90e2f279d71b620b03cf2
mjconl6291 <mjconl6291@gmail.com>
2023-01-08 20:28:52 -0600

    baseline

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -1,15 +1,15 @@
-#!/bin/bash 
+# #!/bin/bash 
 
->&2 echo "Flyway Info: Seeding with data"
+# >&2 echo "Flyway Info: Seeding with data"
 
-base_data=`ls /seed/base-data/*sql`
+# base_data=`ls /seed/base-data/*sql`
 
-write_pg() {
-    arr=$1
-    for filename in "${arr[@]}"
-    do
-        PGPASSWORD=$POSTGRES_PASSWORD  psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "dev-ins-validation-svc" < ${filename}
-    done
-}
+# write_pg() {
+#     arr=$1
+#     for filename in "${arr[@]}"
+#     do
+#         PGPASSWORD=$POSTGRES_PASSWORD  psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "dev-ins-validation-svc" < ${filename}
+#     done
+# }
 
-write_pg $base_data
+# write_pg $base_data
