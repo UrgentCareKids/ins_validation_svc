c26141afae952ff1b778359cc96a7a2ef99ae2e1
mjconl6291 <mjconl6291@gmail.com>
2023-01-08 23:53:32 -0600

    baseline

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 set -e
 JVM_OPTS="-Xms4096M -Xmx1GB"
 sleep 20
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "dev-ins-validation-svc" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "ins_validation_svc" -c '\q'; do
   >&2 echo "Flyway Info: Postgres is unavailable - sleeping"
   sleep 20
 done
