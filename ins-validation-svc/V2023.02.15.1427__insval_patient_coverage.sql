967a455df303fcceb2b39b4c08287e59fec91a84
AnnaStr805 <anna.streichhardt@gmx.de>
2023-02-15 08:30:30 -0600

    insval_patient_coverage

---
+++ ins-validation-svc/V2023.02.15.1427__insval_patient_coverage.sql
@@ -0,0 +1,9 @@
+
+--drop view if exists public.insval_patient_coverage;
+
+CREATE OR REPLACE VIEW public.insval_patient_coverage
+as
+
+select wpj.plantype , ipl.ins_name, ipl.carrier_code, ipl.is_visible, ipl.is_active, wpj.* from wave_parsed_json wpj
+left join ins_cx ic on ic.ext_id = wpj.payercode and ic.ext_source = 'WAVE'
+left join ins_pri_list ipl on ipl.pri_ins_id = ic.pri_ins_id ;
